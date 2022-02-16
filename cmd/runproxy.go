0d2904fa86fc4f40ca54f153b1352f31203d92c8
conni <cbuschka@gmail.com>
2022-02-16 08:57:23 +0100

    runproxy returns exit code 1 in case of error now.

--- cmd/runproxy.go
+++ cmd/runproxy.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"github.com/cbuschka/go-runproxy/internal"
+	"log"
+	"os"
 )
 
 func main() {
-	internal.Run()
+	err := internal.Run()
+	if err != nil {
+		log.Printf("failed: %v", err)
+		os.Exit(1)
+	}
 }
